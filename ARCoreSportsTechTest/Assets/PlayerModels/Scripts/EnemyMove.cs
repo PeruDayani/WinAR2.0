8359eaa365893e9376b8d6ba9aeb6afd8a2a0820
jeshlee121 <jessxlee@berkeley.edu>
2019-05-01 10:52:49 -0700

    WinAR project

---
+++ ARCoreSportsTechTest/Assets/PlayerModels/Scripts/EnemyMove.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    public  GameObject enemyOnGround;
+    public static int i = 0;
+   
+    CharacterController charCont;
+    public GameObject ARCamera;
+    public static Vector3 dir;
+    public static bool ReadyToMove = false;
+    public Animator anim;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+    }
+    public void Ready()
+    {
+
+
+        dir = ARCamera.transform.position - enemyOnGround.transform.position;
+        ReadyToMove = true;
+        anim.SetTrigger("Run");
+        
+
+
+
+    }
+}
---
+++ ARCoreSportsTechTest/Assets/PlayerModels/Scripts/MoveTowardsCamera.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTowardsCamera : MonoBehaviour
+{
+   // public bool charEnabled = false;
+    public Animator anim;
+    public GameObject ARCamera;
+    public CharacterController charCont;
+    bool IsResize = false;
+
+    
+    void Start()
+    {
+        charCont = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (EnemyMove.ReadyToMove)
+        {
+            charCont.Move(EnemyMove.dir *0.5f* Time.deltaTime);
+            //anim.Play("Run");
+        }
+        this.transform.LookAt(new Vector3( ARCamera.transform.position.x, 0, ARCamera.transform.position.z));
+
+
+        if (IsResize)
+        {
+            Vector3 temp = this.transform.localScale;
+            Vector3 tempPos = this.transform.position;
+            tempPos.y += 0.5f * Time.deltaTime;
+            temp.x += 0.5f * Time.deltaTime;
+            temp.y += 0.5f * Time.deltaTime;
+            temp.z += 0.5f * Time.deltaTime;
+            this.transform.localScale = temp;
+            this.transform.position = tempPos;
+        }
+       
+
+            
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        // if (other.gameObject.CompareTag("CamPosition"))
+        charCont.enabled = false;
+        anim.SetTrigger("Tackle");
+
+        StartCoroutine(DestroyEnemy());
+       
+
+
+    }
+    public  void DirectionOfPlayer()
+    {
+      /*  this.GetComponent<CharacterController>().enabled = true;
+        anim = GetComponent<Animator>();
+        anim.SetTrigger("Run");
+        GameObject ARCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        EnemyMove.dir = ARCamera.transform.position - transform.position;
+        */
+    }
+    public void Resize(bool b)
+    {
+        IsResize = b;
+    }
+    IEnumerator DestroyEnemy()
+    {
+        yield return new WaitForSeconds(3f);
+        this.gameObject.SetActive(false);
+    }
+}
